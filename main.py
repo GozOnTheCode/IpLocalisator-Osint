616e7a79a505fd9d5be214db587e056a6c4c146a
GozOnTheCode <79410059+GozOnTheCode@users.noreply.github.com>
2022-08-26 21:50:38 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,70 @@
+import json
+from urllib import response
+import requests
+import os
+from tabulate import tabulate
+
+os.system("cls")
+os.system("title " + "☠️ M0NEGASQUE TOOLS ☠️")
+
+print("This tool, developed by M0NEGASQUE, is a tool that makes it very\neasy to locate an ip address anywhere in the world. Easy to\nuse and very basic tool")
+print('''
+ ███▄ ▄███▓ ███▄    █ ▓█████   ▄████  ▄▄▄        ██████   █████   █    ██ ▓█████ 
+▓██▒▀█▀ ██▒ ██ ▀█   █ ▓█   ▀  ██▒ ▀█▒▒████▄    ▒██    ▒ ▒██▓  ██▒ ██  ▓██▒▓█   ▀ 
+▓██    ▓██░▓██  ▀█ ██▒▒███   ▒██░▄▄▄░▒██  ▀█▄  ░ ▓██▄   ▒██▒  ██░▓██  ▒██░▒███   
+▒██    ▒██ ▓██▒  ▐▌██▒▒▓█  ▄ ░▓█  ██▓░██▄▄▄▄██   ▒   ██▒░██  █▀ ░▓▓█  ░██░▒▓█  ▄ 
+▒██▒   ░██▒▒██░   ▓██░░▒████▒░▒▓███▀▒ ▓█   ▓██▒▒██████▒▒░▒███▒█▄ ▒▒█████▓ ░▒████▒
+░ ▒░   ░  ░░ ▒░   ▒ ▒ ░░ ▒░ ░ ░▒   ▒  ▒▒   ▓▒█░▒ ▒▓▒ ▒ ░░░ ▒▒░ ▒ ░▒▓▒ ▒ ▒ ░░ ▒░ ░
+░  ░      ░░ ░░   ░ ▒░ ░ ░  ░  ░   ░   ▒   ▒▒ ░░ ░▒  ░ ░ ░ ▒░  ░ ░░▒░ ░ ░  ░ ░  ░
+░      ░      ░   ░ ░    ░   ░ ░   ░   ░   ▒   ░  ░  ░     ░   ░  ░░░ ░ ░    ░   
+       ░            ░    ░  ░      ░       ░  ░      ░      ░       ░        ░  ░
+                                                                                 
+▄▄▄█████▓ ▒█████   ▒█████   ██▓      ██████                                      
+▓  ██▒ ▓▒▒██▒  ██▒▒██▒  ██▒▓██▒    ▒██    ▒                                      
+▒ ▓██░ ▒░▒██░  ██▒▒██░  ██▒▒██░    ░ ▓██▄                                        
+░ ▓██▓ ░ ▒██   ██░▒██   ██░▒██░      ▒   ██▒                                     
+  ▒██▒ ░ ░ ████▓▒░░ ████▓▒░░██████▒▒██████▒▒                                     
+  ▒ ░░   ░ ▒░▒░▒░ ░ ▒░▒░▒░ ░ ▒░▓  ░▒ ▒▓▒ ▒ ░                                     
+    ░      ░ ▒ ▒░   ░ ▒ ▒░ ░ ░ ▒  ░░ ░▒  ░ ░                                     
+  ░      ░ ░ ░ ▒  ░ ░ ░ ▒    ░ ░   ░  ░  ░                                       
+             ░ ░      ░ ░      ░  ░      ░                                       
+                                                             ''')
+
+while True:
+
+    print("\n")
+    ip_demande = input("Enter ip adress : ")
+
+    response = requests.get(f'http://ip-api.com/json/{ip_demande}')
+
+    the_reponse = response.json()
+
+    kil = ["ip", "region", "city"]
+
+    ip_value = the_reponse['query']
+    region_value = the_reponse['regionName']
+    city_value = the_reponse['city']
+    zip_value = the_reponse['zip']
+    as_value = the_reponse['as']
+    country_value = the_reponse['country']
+
+    value_tableau = [
+        {
+        'IP': f'{ip_value}',
+        'Country': f'{country_value}',
+        'Region': f'{region_value}',
+        'City': f'{city_value}',
+        'Zip': f'{zip_value}', 
+        'Operator': f'{as_value}'
+        },
+    ]
+
+    for key, value in the_reponse.items():
+        value = value
+        print(tabulate(value_tableau, headers='keys', tablefmt="grid"))
+        break
+
+
+
+
+
