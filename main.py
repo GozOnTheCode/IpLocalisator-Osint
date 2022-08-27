826ee398512ec710c0cd2bb3545fe6c30d407130
GozOnTheCode <79410059+GozOnTheCode@users.noreply.github.com>
2022-08-27 12:15:53 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
+# Update 27/08/2022
+
 import json
+from multiprocessing.resource_sharer import stop
 from urllib import response
 import requests
 import os
@@ -32,37 +35,47 @@ print('''
 
 while True:
 
-    print("\n")
-    ip_demande = input("Enter ip adress : ")
+    try:
+      print("\n")
+      ip_demande = input("Enter ip adress : ")
 
-    response = requests.get(f'http://ip-api.com/json/{ip_demande}')
+      if ip_demande.isalpha():
+        print("Please enter an ip and not letters !")
+      else:
+        response = requests.get(f'http://ip-api.com/json/{ip_demande}')
 
-    the_reponse = response.json()
+        the_reponse = response.json()
 
-    kil = ["ip", "region", "city"]
+        kil = ["ip", "region", "city"]
 
-    ip_value = the_reponse['query']
-    region_value = the_reponse['regionName']
-    city_value = the_reponse['city']
-    zip_value = the_reponse['zip']
-    as_value = the_reponse['as']
-    country_value = the_reponse['country']
+        ip_value = the_reponse['query']
+        region_value = the_reponse['regionName']
+        city_value = the_reponse['city']
+        zip_value = the_reponse['zip']
+        as_value = the_reponse['as']
+        country_value = the_reponse['country']
 
-    value_tableau = [
-        {
-        'IP': f'{ip_value}',
-        'Country': f'{country_value}',
-        'Region': f'{region_value}',
-        'City': f'{city_value}',
-        'Zip': f'{zip_value}', 
-        'Operator': f'{as_value}'
-        },
-    ]
+        value_tableau = [
+            {
+            'IP': f'{ip_value}',
+            'Country': f'{country_value}',
+            'Region': f'{region_value}',
+            'City': f'{city_value}',
+            'Zip': f'{zip_value}', 
+            'Operator': f'{as_value}'
+            },
+        ]
 
-    for key, value in the_reponse.items():
-        value = value
-        print(tabulate(value_tableau, headers='keys', tablefmt="grid"))
-        break
+        for key, value in the_reponse.items():
+            value = value
+            print(tabulate(value_tableau, headers='keys', tablefmt="grid"))
+            break
+    except:
+      print("An error has occurred !")
+
+
+
+
 
 
 
